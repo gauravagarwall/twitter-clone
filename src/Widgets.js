4e14db9c79d1e724c8058e49bc7e166e1b1b06f5
Gaurav <web.gaurav@agarwal.club>
2022-04-04 14:49:27 +0530

    Widget Completed

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./Widgets.css";
+import SearchIcon from "@mui/icons-material/Search";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="GrvAgarwal1"
+          options={{ height: 500 }}
+        />
+      </div>
     </div>
   );
 }
